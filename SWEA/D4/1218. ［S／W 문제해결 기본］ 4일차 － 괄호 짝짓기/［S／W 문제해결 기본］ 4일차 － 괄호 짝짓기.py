cd334178c0b9bed77a631740314b6ef477bcc000
kaeng2 <109322466+kaeng2@users.noreply.github.com>
2022-08-16 15:34:59 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 141 ms, Memory: 56,672 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
@@ -1,24 +1,30 @@
+# 괄호 문자열이 유효한지 확인하는 함수
+def is_valid(string):
+    p_lst = [['(', ')'], ['{', '}'], ['<', '>'], ['[', ']']]
+    cnt_lst = [0] * 4
+    for i in range(4):
+        for j in string:
+            # 해당 글자가 여는 괄호라면 +1
+            if j == p_lst[i][0]:
+                cnt_lst[i] += 1
+            # 닫는 괄호라면 -1
+            elif j == p_lst[i][1]:
+                cnt_lst[i] -= 1
+            # cnt가 음수가 되는 순간 0 반환
+            if cnt_lst[i] < 0:
+                return 0
+        # string의 모든 글자를 검사했을 때 cnt가 0이 아니라면 0 반환
+        if cnt_lst[i] != 0:
+            return 0
+    # 모두 통과한다면 1 반환
+    return 1
+
 # 각 테스트 케이스마다
 for t in range(1, 11):
     # 테스트 케이스 길이
     N = int(input())
     # 괄호 문자열 입력
     string = input().replace('()', '').replace('{}', '').replace('<>', '').replace('[]', '')
-    p_dict = {'(': ')', '{': '}', '<': '>', '[': ']'}
-    i = 0
-    # 괄호 짝이 있으면 둘 다 제거
-    while string:
-        k = string[i]
-        # 닫는 괄호일 경우
-        if p_dict.get(k, 0) == 0:
-            print(f'#{t} 0')
-            break
-        # 여는 괄호지만 짝이 없는 경우
-        elif string[i+1:].count(p_dict[k]) == 0:
-            print(f'#{t} 0')
-            break
-        # 이 외의 경우에는 짝을 지우자
-        else:
-            string = string.replace(k, '', 1).replace(p_dict[k], '', 1)
-    else:
-        print(f'#{t} 1')
+    # 결과 출력
+    result = is_valid(string)
+    print(f'#{t} {result}')
