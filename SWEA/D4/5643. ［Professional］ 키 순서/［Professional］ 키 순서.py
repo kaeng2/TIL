0b31fa22a7b161c89a61503a28af3231b91f5b7b
kaeng2 <109322466+kaeng2@users.noreply.github.com>
2022-08-18 17:22:09 +0900

    [D4] Title: [Professional] 키 순서, Time: 7,675 ms, Memory: 107,364 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
@@ -0,0 +1,34 @@
+# 테스트 케이스 개수
+T = int(input())
+# 각 테스트 케이스마다
+for t in range(1, T+1):
+    # 학생 수, 비교 횟수
+    N, M = int(input()), int(input())
+    # 인접 행렬
+    link = [[0] * (N+1) for _ in range(N+1)]
+    for _ in range(M):
+        s, e = map(int, input().split())
+        link[s][e] = 1
+    # 각 학생마다 자기보다 큰 학생을 모두 표시해주기
+    for i in range(1, N+1):
+        # 방문 체크
+        visited = [0] * (N+1)
+        while True:
+            # 현재 학생 기준으로 더 크고, 아직 조사하지 않은 학생 번호 리스트
+            search = [x for x in range(1, N+1) if link[i][x] and not visited[x]]
+            if search:
+                for j in search:
+                    visited[j] = 1  # 방문 처리
+                    for k in [y for y in range(1, N+1) if link[j][y] and not visited[y]]:
+                        link[i][k] = 1
+            # 조사할 학생이 없다면 종료
+            else:
+                break
+    # 자기보다 더 큰 학생 + 더 작은 학생 = (자기를 제외한 모든 학생) 이면 cnt를 1 올려줌
+    cols = list(zip(*link))
+    cnt = 0
+    for i in range(1, N+1):
+        if link[i].count(1) + cols[i].count(1) == N-1:
+            cnt += 1
+    # 출력
+    print(f'#{t} {cnt}')
