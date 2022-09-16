2cd046b7a875ed16271066fdb69042dbc9357479
kaeng2 <109322466+kaeng2@users.noreply.github.com>
2022-09-17 04:02:56 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 372 ms, Memory: 58508 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -10,6 +10,13 @@ import heapq
 정점 선택을 기반으로 하는 Prim MST 알고리즘으로 풀이했다.
 '''
 
+'''
+최소 스패닝 트리!
+
+정점 개수는 최대 10,000개이고 간선 개수는 최대 100,000개라서
+정점 선택을 기반으로 하는 Prim MST 알고리즘으로 풀이했다.
+'''
+
 V, E = map(int, input().split())            # 정점 개수, 간선 개수
 link = [[] for _ in range(V)]               # link[i] = i번 정점에서 갈 수 있는 (비용, 정점) 목록
 for _ in range(E):
@@ -19,13 +26,16 @@ for _ in range(E):
 
 ans = 0
 visited = [0] * V                       # 방문 배열
+cost = [1000001] * V                    # cost[i] = i번 노드로 오는 간선 중 최소 비용
 Q = [(0, 0)]                            # 0번 정점에서 비용 0으로 시작
 while Q:
-    cost, now = heapq.heappop(Q)        # 연결된 정점 중에 비용이 최소인 정점으로 이동
+    c, now = heapq.heappop(Q)           # 연결된 정점 중에 비용이 최소인 정점으로 이동
     if visited[now]:                    # 이미 방문한 곳이라면 pass
         continue
-    ans += cost                         # 전체 비용에 추가
+    ans += c                            # 전체 비용에 추가
     visited[now] = 1                    # 방문 처리
-    for nxt in link[now]:               # 현재 정점에서 연결된 정점 전부 enqueue
-        heapq.heappush(Q, nxt)
+    for c, nxt in link[now]:               # 현재 정점에서 연결된 정점 중에
+        if not visited[nxt] and cost[nxt] > c:  # 지금까지의 최소 비용보다 더 낮은 비용으로 갈 수 있는 미방문 정점이 있으면
+            heapq.heappush(Q, (c, nxt))             # enqueue
+            cost[nxt] = c                           # 최소 비용 갱신
 print(ans)
