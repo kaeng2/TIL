eeba08f0f6fe038e5877dab895023ccf36ddf0a1
kaeng2 <109322466+kaeng2@users.noreply.github.com>
2022-09-28 18:41:02 +0900

    [Gold V] Title: 두 용액, Time: 172 ms, Memory: 42312 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+'''
+투 포인터!
+시작 포인터와 끝 포인터를 양 끝단에 두고 시작한다.
+혼합 용액의 특성값과 0을 비교한 결과에 따라 각 포인터를 가운데 방향으로 이동시킨다.
+'''
+N = int(input())
+solutions = sorted(list(map(int, input().split())))         # 정렬하여 저장
+s, e = 0, N-1                                               # 양 끝의 두 용액부터 검사
+ans = (2000000001, s, e)
+while s < e:                                                # s == e를 만족하면 종료
+    mix = solutions[s] + solutions[e]                           # 혼합 용액의 특성값
+    ans = min(ans, (abs(mix), solutions[s], solutions[e]))      # 특성값이 0에 더 가까운 두 용액으로 정답 갱신
+    if mix < 0:                                                 # 0보다 작으면
+        mix -= solutions[s]                                         # 시작 포인터 옮기기
+        s += 1
+    elif mix > 0:                                               # 0보다 크면
+        e -= 1                                                      # 끝 포인터 옮기기
+        mix -= solutions[e]
+    else:                                                       # 특성값이 0이면
+        break                                                       # 종료
+print(*ans[1:])
