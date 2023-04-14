903d82ab4c43c063147f5feabf883a50ac244e99
kaeng2 <109322466+kaeng2@users.noreply.github.com>
2023-04-14 17:09:48 +0900

    [Silver III] Title: 계단 오르기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,23 +1,13 @@
 import sys
-sys.stdin.readline
+input = sys.stdin.readline
 
-'''
-dp[i] = i번째 계단까지 올랐을 때, i번째 계단을 밟는 경우의 최대값
-i번째 계단을 밟는 경우는 다음과 같이 나뉜다
-    1. i-1번째 계단을 안 밟고 i-2번째 계단을 밟는 경우                       -> dp[i-2]
-    2. i-1번째 계단을 밟고 i-2번째 계단을 안 밟고 i-3번째 계단을 밟는 경우      -> scores[i-1] + dp[i-3]
-따라서 dp[i]는 위의 두 가지 경우 중 최대값에 scores[i]를 더한 값이 된다.
-'''
+n = int(input())
+stairs = [0, 0, 0] + [int(input()) for _ in range(n)]
+max_scores_at = [0 for _ in range(n+3)]
+for i in range(3, n+3):
+    max_scores_at[i] = stairs[i] + max(
+        max_scores_at[i-2],
+        stairs[i-1] + max_scores_at[i-3]
+    )
 
-N = int(input())                                # 계단의 개수
-scores = [int(input()) for _ in range(N)]       # 각 계단의 점수
-# 계단이 3개 이상인 경우
-if N > 2:
-    dp = [0] * N
-    dp[0:3] = [scores[0], scores[0] + scores[1], max(scores[0], scores[1]) + scores[2]]
-    for i in range(3, N):
-        dp[i] = max(dp[i-2], dp[i-3] + scores[i-1]) + scores[i]
-    print(dp[N-1])
-# 계단이 3개 미만인 경우
-else:
-    print(sum(scores))
+print(max_scores_at[-1])
